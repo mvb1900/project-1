bd2b523b2c193fd53b71b475b825628d7df316d1
Your Name <you@example.com>
2023-10-19 16:48:56 +0700

    update todolist app

--- app_todolist/views/task.py
+++ app_todolist/views/task.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from app_todolist.models import Task
 from app_todolist.serializers.task import TaskSerializer
 from rest_framework.viewsets import ViewSet
 
 
-class TaskAPIView(ViewSet, generics.ListAPIView, generics.CreateAPIView, generics.RetrieveAPIView, generics.DestroyAPIView,
+class TaskAPIView(ViewSet, generics.ListAPIView, generics.CreateAPIView, generics.RetrieveAPIView,
+                  generics.DestroyAPIView,
                   generics.UpdateAPIView):
 
     def get_serializer_class(self):
@@ -13,3 +16,21 @@ class TaskAPIView(ViewSet, generics.ListAPIView, generics.CreateAPIView, generic
 
     def get_queryset(self):
         return Task.objects.all()
+
+    @action(methods=['POST'], detail=True, url_path='completed')
+    def set_task_completed(self, request, pk=None):
+        obj = self.get_object()
+        obj.is_completed = True
+        obj.save()
+        serializer = TaskSerializer(obj)
+
+        return Response(serializer.data)
+
+    @action(methods=['POST'], detail=True, url_path='uncompleted')
+    def set_task_uncompleted(self, request, pk=None):
+        obj = self.get_object()
+        obj.is_completed = False
+        obj.save()
+        serializer = TaskSerializer(obj)
+
+        return Response(serializer.data)
--- project_1/urls.py
+++ project_1/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app_todolist.urls"), name="app_todolist"),
-    path("api/", include("app_api_gateway.urls"), name="app_api_gateway")
+    path("api/", include("app_api_gateway.urls"), name="app_api_gateway"),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
